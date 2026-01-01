b88f049e119c29e744bf50006647eed089e201a8
munish <munish28297@gmail.com>
2026-01-01 22:55:48 +0530

    add axiosinstance

--- src/components/utitlities/axiosInstance.js
+++ src/components/utitlities/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const DB_URL = import.meta.env.VITE_DB_URL || "https://chat-server-seven-blue.vercel.app/";
+const DB_URL = import.meta.env.VITE_DB_URL || "https://chat-server-seven-blue.vercel.app/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL: DB_URL,
   withCredentials: true,
   headers: {
-    ContentType: "application/json",
+    "Content-Type": "application/json",
   },
 });
